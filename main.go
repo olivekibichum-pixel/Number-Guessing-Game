bb90e2976e286b146d4249676922695b30c5ffc9
olivekibichum-pixel <olivekibichum@gmail.com>
2026-09-23 21:00:51 +0300

    Add exiting game and limited input to the range

--- main.go
+++ main.go
@@ -6,26 +6,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to the Number Guessing Game!")
+	fmt.Println("Welcome to the Number Guessing Game! \nThe number range is 1-100. \nTo EXIT the game, enter 0. \nHave Fun!")
 
 	var secretNumber = rand.Intn(100) + 1
 
 	for {
 		var guess int
-		var difference = (guess - secretNumber)
 
-		fmt.Print("Guess a number: ")
+		fmt.Print("Guess the number: ")
 		fmt.Scan(&guess)
+
+		var difference int = (guess - secretNumber)
 
 		if difference < 0 {
 			difference = -difference
 		}
 
-		if difference <= 3 {
-			fmt.Println("Warmer!")
+		if guess == 0 {
+			break
+		} else if guess > 100 {
+			fmt.Println("Error")
 		} else if guess == secretNumber {
 			fmt.Println("Correct!")
 			break
+		} else if difference <= 3 {
+			fmt.Println("Warmer!")
 		} else if guess > secretNumber {
 			fmt.Println("Too high!")
 		} else {
